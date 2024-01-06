e651e1cdb1f2d61ea6d3fab904d22ecd651ad221
Max You <JianzhongMax.You@ibm.com>
2024-01-06 15:28:06 -0500

    save individual cam into its correspnding folder

--- Multi_CAM.py
+++ Multi_CAM.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--models', type=str, default='resnet50',
                     help='resnet50 or vgg16 or vgg19')
-parser.add_argument('--target_layer', type=str, default='layer4',
+parser.add_argument('--target_layer', type=str, default='layer3',
                     help='target_layer')
 parser.add_argument('--target_class', type=int, default=None,
                     help='target_class')
@@ -68,8 +68,18 @@ target_layer.register_backward_hook(backward_hook)
 # path_s = os.listdir('./picture')
 path_s = os.listdir('./sample-imagenet')
 
+def save_cam(cam, image, save_path):
+    plt.imshow((cam),cmap='seismic')
+    plt.imshow(image, alpha=.5)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+    plt.show()
+    plt.savefig(save_path)
+    plt.clf()
+    plt.close()
 
-for path in path_s[:10]:
+for path in path_s[:200]:
     # img_path_long = './picture/{}'.format(path)
     img_path_long = './sample-imagenet/{}'.format(path)
     img = cv2.imread(img_path_long,1)
@@ -87,7 +97,7 @@ for path in path_s[:10]:
 
     print(index2class[maxindex])
     # save_path = './results/{}_{}_{}_{}'.format(index2class[maxindex][:10], 'XRelevance' if xMode else 'Relevance', args.target_layer, img_path_long.split('/')[-1])
-    save_path = './results-sample-imagenet/{}_{}'.format(args.target_layer, img_path_long.split('/')[-1])
+    # save_path = './results-sample-imagenet/{}_{}'.format(args.target_layer, img_path_long.split('/')[-1])
 
     output[:, maxindex].sum().backward(retain_graph=True)
     activation = value['activations']  # [1, 2048, 7, 7]
@@ -113,15 +123,15 @@ for path in path_s[:10]:
     xgrad_cam = cv2.resize(xgrad_cam, (224, 224))
 
     # grad-cam++
-    # alpha_numer = gradient_2
-    # alpha_denom = 2 * gradient_2 + torch.sum(activation * gradient_3, axis=(2, 3), keepdims=True)  # + 1e-2
-    # alpha = alpha_numer / alpha_denom
-    # w = torch.sum(alpha * torch.clamp(gradient, 0), axis=(2, 3), keepdims=True)
-    # grad_campp = activation * w
-    # grad_campp = torch.sum(grad_campp, dim=(0, 1))
-    # grad_campp = torch.clamp(grad_campp, min=0)
-    # grad_campp = grad_campp.data.cpu().numpy()
-    # grad_campp = cv2.resize(grad_campp, (224, 224))
+    alpha_numer = gradient_2
+    alpha_denom = 2 * gradient_2 + torch.sum(activation * gradient_3, axis=(2, 3), keepdims=True)  # + 1e-2
+    alpha = alpha_numer / alpha_denom
+    w = torch.sum(alpha * torch.clamp(gradient, 0), axis=(2, 3), keepdims=True)
+    grad_campp = activation * w
+    grad_campp = torch.sum(grad_campp, dim=(0, 1))
+    grad_campp = torch.clamp(grad_campp, min=0)
+    grad_campp = grad_campp.data.cpu().numpy()
+    grad_campp = cv2.resize(grad_campp, (224, 224))
 
     # xrelevance-cam
     XR_CAM = tensor2image(XR_CAM)
@@ -129,90 +139,87 @@ for path in path_s[:10]:
     # relevance-cam
     R_CAM = tensor2image(R_CAM)
 
-    fig = plt.figure(figsize=(10, 10))
-    plt.subplots_adjust(bottom=0.01)
+    # create file directory if not exists
+    save_path_parent_dir  = './results-sample-imagenet/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer)
+    if not os.path.exists(save_path_parent_dir):
+        os.makedirs(save_path_parent_dir)
+    # save the cams
+    save_path_relevance_cam = './results-sample-imagenet/{}/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer, 'RelevanceCAM')
+    save_path_xrelevance_cam = './results-sample-imagenet/{}/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer, 'XRelevanceCAM')
+    save_path_xgrad_cam = './results-sample-imagenet/{}/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer, 'XGradCAM')
+    save_path_grad_cam = './results-sample-imagenet/{}/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer, 'GradCAM')
+    save_path_gradcam_pp = './results-sample-imagenet/{}/{}/{}'.format(img_path_long.split('/')[-1], args.target_layer, 'GradCAM++')
 
-    plt.subplot(2, 5, 1)
-    plt.imshow(img_show)
-    plt.title('Original')
-    plt.axis('off')
+    save_cam(R_CAM, img_show, save_path_relevance_cam)
+    save_cam(XR_CAM, img_show, save_path_xrelevance_cam)
+    save_cam(xgrad_cam, img_show, save_path_xgrad_cam)
+    save_cam(grad_campp, img_show, save_path_gradcam_pp)
+    save_cam(grad_cam, img_show, save_path_grad_cam)
 
-    plt.subplot(2, 5, 1 + 5)
-    plt.imshow(img_show)
-    plt.axis('off')
+    # fig = plt.figure(figsize=(10, 10))
+    # plt.subplots_adjust(bottom=0.01)
 
-    plt.subplot(2, 5, 2)
-    plt.imshow((grad_cam),cmap='seismic')
-    plt.imshow(img_show, alpha=.5)
-    plt.title('GradCAM', fontsize=15)
-    plt.axis('off')
+    # plt.subplot(2, 5, 1)
+    # plt.imshow(img_show)
+    # plt.title('Original')
+    # plt.axis('off')
 
-    plt.subplot(2, 5, 2 + 5)
-    plt.imshow(img_show*threshold(grad_cam)[...,np.newaxis])
-    plt.title('GradCAM', fontsize=15)
-    plt.axis('off')
+    # plt.subplot(2, 5, 1 + 5)
+    # plt.imshow(img_show)
+    # plt.axis('off')
+
+    # plt.subplot(2, 5, 2)
+    # plt.imshow((grad_cam),cmap='seismic')
+    # plt.imshow(img_show, alpha=.5)
+    # plt.title('GradCAM', fontsize=15)
+    # plt.axis('off')
+
+    # plt.subplot(2, 5, 2 + 5)
+    # plt.imshow(img_show*threshold(grad_cam)[...,np.newaxis])
+    # plt.title('GradCAM', fontsize=15)
+    # plt.axis('off')
 
     # plt.subplot(2, 5, 3)
-    # plt.imshow((grad_campp),cmap='seismic')
+    # plt.imshow((xgrad_cam),cmap='seismic')
     # plt.imshow(img_show, alpha=.5)
-    # plt.title('Grad CAM++', fontsize=15)
+    # plt.title('XGradCAM', fontsize=15)
     # plt.axis('off')
 
     # plt.subplot(2, 5, 3 + 5)
-    # plt.imshow(img_show*threshold(grad_campp)[...,np.newaxis])
-    # plt.title('Grad CAM++', fontsize=15)
+    # plt.imshow(img_show*threshold(xgrad_cam)[...,np.newaxis])
+    # plt.title('XGradCAM', fontsize=15)
     # plt.axis('off')
 
-    plt.subplot(2, 5, 3)
-    plt.imshow((xgrad_cam),cmap='seismic')
-    plt.imshow(img_show, alpha=.5)
-    plt.title('XGradCAM', fontsize=15)
-    plt.axis('off')
-
-    plt.subplot(2, 5, 3 + 5)
-    plt.imshow(img_show*threshold(xgrad_cam)[...,np.newaxis])
-    plt.title('XGradCAM', fontsize=15)
-    plt.axis('off')
-
-    # TODO: replace this with XGradCAM
     # plt.subplot(2, 5, 4)
-    # plt.imshow((score_map),cmap='seismic')
+    # plt.imshow((R_CAM),cmap='seismic')
     # plt.imshow(img_show, alpha=.5)
-    # plt.title('Score_CAM', fontsize=15)
+    # plt.title('RelevanceCAM', fontsize=15)
     # plt.axis('off')
 
     # plt.subplot(2, 5, 4 + 5)
-    # plt.imshow(img_show*threshold(score_map)[...,np.newaxis])
-    # plt.title('Score_CAM', fontsize=15)
+    # plt.imshow(img_show*threshold(R_CAM)[...,np.newaxis])
+    # plt.title('RelevanceCAM', fontsize=15)
     # plt.axis('off')
 
-    plt.subplot(2, 5, 4)
-    plt.imshow((R_CAM),cmap='seismic')
-    plt.imshow(img_show, alpha=.5)
-    plt.title('RelevanceCAM', fontsize=15)
-    plt.axis('off')
+    # plt.subplot(2, 5, 5)
+    # plt.imshow((XR_CAM),cmap='seismic')
+    # plt.imshow(img_show, alpha=.5)
+    # plt.title('XRelevanceCAM', fontsize=15)
+    # plt.axis('off')
 
-    plt.subplot(2, 5, 4 + 5)
-    plt.imshow(img_show*threshold(R_CAM)[...,np.newaxis])
-    plt.title('RelevanceCAM', fontsize=15)
-    plt.axis('off')
+    # plt.subplot(2, 5, 5 + 5)
+    # plt.imshow(img_show*threshold(XR_CAM)[...,np.newaxis])
+    # plt.title('XRelevanceCAM', fontsize=15)
+    # plt.axis('off')
 
-    plt.subplot(2, 5, 5)
-    plt.imshow((XR_CAM),cmap='seismic')
-    plt.imshow(img_show, alpha=.5)
-    plt.title('XRelevanceCAM', fontsize=15)
-    plt.axis('off')
-
-    plt.subplot(2, 5, 5 + 5)
-    plt.imshow(img_show*threshold(XR_CAM)[...,np.newaxis])
-    plt.title('XRelevanceCAM', fontsize=15)
-    plt.axis('off')
-
-    plt.tight_layout()
-    plt.draw()
-    # plt.waitforbuttonpress()
-    plt.savefig(save_path)
-    plt.clf()
-    plt.close()
+    # plt.tight_layout()
+    # plt.draw()
+    # # plt.waitforbuttonpress()
+    # plt.savefig(save_path)
+    # plt.clf()
+    # plt.close()
 
 print('Done')
+
+
+
